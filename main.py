47ed169203b04aa34d89966a72fdb2192e66c4ba
satvikwadhai777 <satvikwadhai777@gmail.com>
2026-07-07 07:27:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import secrets
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = "".join(secrets.choice(characters) for i in range(length))
+
+print("Your secure password is:", password)
